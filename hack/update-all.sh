beb19b4779c10bc0ee62283dc189eecc0cb5bfd1
Carlisia <carlisiac@vmware.com>
2019-04-24 11:05:23 -0700

    Revert
    Signed-off-by: Carlisia <carlisiac@vmware.com>

--- hack/update-all.sh
+++ hack/update-all.sh
@@ -19,7 +19,7 @@ HACK_DIR=$(dirname "${BASH_SOURCE}")
 echo "Running all update scripts"
 
 for f in ${HACK_DIR}/update-*.sh; do
-  if [[ $f = "${HACK_DIR}/update-all.sh" ]]; then
+  if [[ $f == "${HACK_DIR}/update-all.sh" ]]; then
     continue
   fi
   $f
