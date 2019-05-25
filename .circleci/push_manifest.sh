ea104067590d09dc9a6fcb856370e479889d1a0e
Jesse Stuart <hi@jessestuart.com>
2019-05-25 17:45:03 -0400

    [commit message skipped]

--- .circleci/push_manifest.sh
+++ .circleci/push_manifest.sh
@@ -1,14 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 . $BASH_ENV
 echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
-manifest-tool push from-args \
-  --platforms linux/amd64,linux/arm,linux/arm64 \
-  --template "$REGISTRY/$IMAGE:$VERSION-ARCH" \
-  --target "$REGISTRY/$IMAGE:$VERSION"
-if [ "${CIRCLE_BRANCH}" == 'master' ]; then
+
+function push_from_args() {
+  local tag=$1
+  local target_image=$2
+  local platforms='linux/amd64,linux/arm,linux/arm64'
+  local template="$REGISTRY/$target_image:$VERSION-ARCH"
+  echo "Pushing manifest with template: $template, target: $REGISTRY/$target_image:$tag"
   manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm,linux/arm64 \
-    --template "$REGISTRY/$IMAGE:$VERSION-ARCH" \
-    --target "$REGISTRY/$IMAGE:latest"
+    --platforms $platforms \
+    --template $template \
+    --target "$REGISTRY/$target_image:$tag"
+}
+
+push_from_args $VERSION 'velero'
+push_from_args $VERSION 'velero-restic-restore-helper'
+
+if [ "${CIRCLE_BRANCH}" == 'master' ]; then
+  push_from_args 'latest' 'velero'
+  push_from_args 'latest' 'velero-restic-restore-helper'
 fi
