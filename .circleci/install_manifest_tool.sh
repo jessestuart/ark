c5b1c368800d0c88814c6dc7557f3cafa81de9cf
Jesse Stuart <hi@jessestuart.com>
2020-02-15 19:23:27 -0500

    chore(deps): Update manifest-tool.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- .circleci/install_manifest_tool.sh
+++ .circleci/install_manifest_tool.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Downloading manifest-tool."
-wget https://github.com/estesp/manifest-tool/releases/download/v0.9.0/manifest-tool-linux-amd64
+MT_VERSION=$(curl https://api.github.com/repos/estesp/manifest-tool/releases | jq '.[].tag_name' -r | head -n1)
+wget https://github.com/estesp/manifest-tool/releases/download/${MT_VERSION}/manifest-tool-linux-amd64
 mv manifest-tool-linux-amd64 /usr/bin/manifest-tool
 chmod +x /usr/bin/manifest-tool
 manifest-tool --version
